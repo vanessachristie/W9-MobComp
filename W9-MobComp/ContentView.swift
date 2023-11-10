cad699c71b37b8c8efda56c5200878dea2663067
vanessachristie <95264849+vanessachristie@users.noreply.github.com>
2023-11-10 08:00:22 +0700

    Initial Commit

---
+++ W9-MobComp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  W9-MobComp
+//
+//  Created by MacBook Pro on 10/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ W9-MobComp/W9_MobCompApp.swift
@@ -0,0 +1,17 @@
+//
+//  W9_MobCompApp.swift
+//  W9-MobComp
+//
+//  Created by MacBook Pro on 10/11/23.
+//
+
+import SwiftUI
+
+@main
+struct W9_MobCompApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
