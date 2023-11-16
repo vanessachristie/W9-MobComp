30f64da463b6a6d4194510eb1987e77eca0d70d6
vanessachristie <95264849+vanessachristie@users.noreply.github.com>
2023-11-16 17:05:33 +0700

    legalities,version,swipe card,sort

--- W9-MobComp/ContentView.swift
+++ W9-MobComp/ContentView.swift
@@ -6,67 +6,297 @@
 //
 
 import SwiftUI
+import Foundation
 
 struct MTGCardView: View {
-    var card: MTGCard
+    var card: [MTGCard]
+    @State private var isShowingVersions = false
+    @State private var isShowingRulings = false
+    @State private var currentIndex: Int
+    @State private var selectedButton: String?
+    
+    init(card: [MTGCard], currentIndex: Int) {
+        self.card = card
+        self._currentIndex = State(initialValue: currentIndex)
+    }
     
     var body: some View {
-        VStack(spacing: 16) {
-            // Display card image
-            AsyncImage(url: URL(string: card.image_uris?.large ?? "")) { phase in
-                switch phase {
-                case .success(let image):
-                    image
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .cornerRadius(15) // Add corner radius for a rounded image
-                case .failure:
-                    Image(systemName: "exclamationmark.triangle")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .foregroundColor(.red)
-                        .cornerRadius(15) // Add corner radius for a rounded image
-                case .empty:
-                    ProgressView()
-                @unknown default:
-                    ProgressView()
+        ScrollView {
+            VStack(spacing: 16) {
+                // Display card image
+                AsyncImage(url: URL(string: card[currentIndex].image_uris?.large ?? "")) { phase in
+                    switch phase {
+                    case .success(let image):
+                        image
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .cornerRadius(15)
+                    case .failure:
+                        Image(systemName: "exclamationmark.triangle")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .foregroundColor(.red)
+                            .cornerRadius(15)
+                    case .empty:
+                        ProgressView()
+                    @unknown default:
+                        ProgressView()
+                    }
+                }
+                .frame(maxHeight: 200)
+                HStack {
+                    Button(action: {
+                        navigateToPreviousCard()
+                    }) {
+                        Image(systemName: "arrow.left.circle")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 30, height: 30)
+                            .foregroundColor(.gray)
+                    }
+                    .padding(.leading, 16)
+                    
+                    Spacer()
+                    
+                    Text(card[currentIndex].name)
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .multilineTextAlignment(.center)
+                        .padding(.horizontal, 16)
+                    
+                    Spacer()
+                    
+                    Button(action: {
+                        navigateToNextCard()
+                    }) {
+                        Image(systemName: "arrow.right.circle")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 30, height: 30)
+                            .foregroundColor(.gray)
+                    }
+                    .padding(.trailing, 16)
+                }
+                .padding(.top, 16)
+                
+                Text(card[currentIndex].type_line)
+                    .fontWeight(.bold)
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal, 16)
+                    .font(.system(size: 20))
+                
+                // Display Oracle Text
+                VStack(alignment: .leading, spacing: 8) {
+                    Text("\(card[currentIndex].oracle_text)")
+                        .font(.body)
+                }
+                .gesture(
+                                DragGesture()
+                                    .onEnded { gesture in
+                                        let swipeThreshold: CGFloat = 50
+                                        if gesture.translation.width > swipeThreshold {
+                                            navigateToPreviousCard()
+                                        } else if gesture.translation.width < -swipeThreshold {
+                                            navigateToNextCard()
+                                        }
+                                    }
+                            )
+                .padding()
+                
+                Spacer()
+                
+            }
+            
+            .padding()
+            .background(Color.white)
+            .cornerRadius(20)
+            .shadow(radius: 5)
+            .padding(.horizontal, 16)
+
+            HStack(spacing: 16) {
+                Button(action: {
+                    isShowingVersions.toggle()
+                    isShowingRulings = false
+                }) {
+                    ZStack {
+
+                        RoundedRectangle(cornerRadius: 10)
+                            .foregroundColor(Color.clear)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 10)
+                                    .stroke(Color.gray, lineWidth: 2)
+                            )
+                            .frame(height: 40)
+                        RoundedRectangle(cornerRadius: 10)
+                            .foregroundColor(isShowingVersions ? Color.red : Color.clear)
+                    }
+                    .frame(height: 40)
+                    .overlay(
+                        Text("Versions")
+                            .foregroundColor(isShowingVersions ? Color.white : Color.gray)
+                    )
+                }
+                
+                Button(action: {
+                    isShowingRulings.toggle()
+                    isShowingVersions = false
+                }) {
+                    ZStack {
+                    
+                        RoundedRectangle(cornerRadius: 10)
+                            .foregroundColor(Color.clear)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 10)
+                                    .stroke(Color.gray, lineWidth: 2)
+                            )
+                            .frame(height: 40)
+                        
+                        RoundedRectangle(cornerRadius: 10)
+                            .foregroundColor(isShowingRulings ? Color.red : Color.clear)
+                    }
+                    .frame(height: 20)
+                    .overlay(
+                        Text("Ruling")
+                            .foregroundColor(isShowingRulings ? Color.white : Color.gray)
+                    )
                 }
             }
-            .frame(maxHeight: 200) // Set a maximum height for the image view
-
-            // Display card name
-            Text(card.name)
-                .font(.title)
-                .fontWeight(.bold)
-                .multilineTextAlignment(.center)
-                .padding(.horizontal, 16)
-
+            .padding(.horizontal, 16)
+            .padding(.top, 16)
             
-            VStack(alignment: .leading, spacing: 8) {
-                Text("Type: \(card.type_line)")
-                    .font(.headline)
-                    .foregroundColor(.blue)
-                Text("Oracle Text: \(card.oracle_text)")
-                    .font(.body)
-            }
-            .padding()
+            if isShowingVersions {
+                VStack {
+                    Text("PRICES")
+                        .fontWeight(.bold)
+                        .foregroundColor(Color.red)
 
-            Spacer()
+                    ForEach(Array(Mirror(reflecting: card[currentIndex].prices ?? [:]).children), id: \.label) { child in
+                        if let label = child.label, let value = child.value as? String {
+                            let formattedKey = label.replacingOccurrences(of: "_", with: " ").capitalized
+                            PriceItem(value: value, key: formattedKey)
+                        }
+                    }
+                    .padding()
+                }
+                .padding()
+                .background(RoundedRectangle(cornerRadius: 10).fill(Color.white))
+                .overlay(
+                    RoundedRectangle(cornerRadius: 10)
+                        .stroke(Color.gray, lineWidth: 1)
+                        .shadow(color: Color.gray.opacity(0.5), radius: 5, x: 0, y: 2)
+                )
+            }
+
+            if isShowingRulings {
+                VStack() {
+                    HStack {
+                        Spacer()
+                        Text("LEGALITIES")
+                            .font(.headline)
+                            .foregroundColor(.red)
+                        Spacer()
+                    }
+                    
+                    if let legalities = card[currentIndex].legalities {
+                        let legalitiesArray = Array(Mirror(reflecting: legalities).children)
+                        let midpoint = legalitiesArray.count / 2
+                        
+                        HStack(spacing: 16) {
+                            VStack(spacing: 4) {
+                                ForEach(0..<midpoint, id: \.self) { index in
+                                
+                                    HStack {
+                                        Button(action: {
+                                      
+                                        }) {
+                                            Text(legalitiesArray[index].value as? String == "legal" ? "Legal" : "Not Legal")
+                                                .foregroundColor(.white)
+                                                .padding(.horizontal, 12)
+                                                .padding(.vertical, 6)
+                                                .frame(width: 100)
+                                                .background((legalitiesArray[index].value as? String == "legal") ? Color.green : Color.gray)
+                                                .cornerRadius(8)
+                                        }
+                                        Spacer()
+                                        Text("\(legalitiesArray[index].label?.capitalized ?? "")")
+                                            .foregroundColor(.black)
+                                    }
+                                    .padding(.top, 4)
+                                }
+                            }
+                            
+
+                            VStack(spacing: 4) {
+                                ForEach(midpoint..<legalitiesArray.count, id: \.self) { index in
+                                    
+                                    HStack {
+                                        Button(action: {
+                                      
+                                        }) {
+                                            Text(legalitiesArray[index].value as? String == "legal" ? "Legal" : "Not Legal")
+                                                .foregroundColor(.white)
+                                                .padding(.horizontal, 12)
+                                                .padding(.vertical, 6)
+                                                .frame(width: 100)
+                                                .background((legalitiesArray[index].value as? String == "legal") ? Color.green : Color.gray)
+                                                .cornerRadius(8)
+                                        }
+                                        Spacer()
+                                        Text("\(legalitiesArray[index].label?.capitalized ?? "")")
+                                            .foregroundColor(.black)
+                                    }
+                                    .padding(.top, 4)
+                                }
+                            }
+                        }
+                        .padding(.horizontal, 16)
+                    }
+                }
+                .padding(.top, 8)
+            }
+            
         }
-        .padding()
-        .background(Color.white)
-        .cornerRadius(20)
-        .shadow(radius: 5)
-        .padding(.horizontal, 16)
+        
     }
+    private func navigateToPreviousCard() {
+           if currentIndex > 0 {
+               currentIndex -= 1
+           }
+           selectedButton = nil
+       }
+
+       private func navigateToNextCard() {
+           if currentIndex < card.count - 1 {
+               currentIndex += 1
+           }
+           selectedButton = nil
+       }
+    struct PriceItem: View {
+        var value: String
+        var key: String
+        
+        var body: some View {
+            HStack {
+                Text("\(key):")
+                    .fontWeight(.bold)
+                
+                Text(value)
+            }
+        }
+    }
+}
+
+extension MTGCardView: Identifiable {
+    var id: UUID { card[currentIndex].id }
 }
 
 
 struct ContentView: View {
     @State private var mtgCards: [MTGCard] = []
     @State private var searchText: String = ""
+    @State private var isAscendingOrder: Bool = true
 
-    let columns: [GridItem] = Array(repeating: .init(.flexible()), count: 3) // Three cards per row
+    let columns: [GridItem] = Array(repeating: .init(.flexible()), count: 3)
 
     var filteredCards: [MTGCard] {
         if searchText.isEmpty {
@@ -75,8 +305,11 @@ struct ContentView: View {
             return mtgCards.filter { $0.name.lowercased().contains(searchText.lowercased()) }
         }
     }
+
     struct SearchBar: View {
         @Binding var text: String
+        @Binding var isAscendingOrder: Bool
+        var sortAction: () -> Void
 
         var body: some View {
             HStack {
@@ -85,29 +318,50 @@ struct ContentView: View {
                     .background(Color(.systemGray6))
                     .cornerRadius(8)
                     .padding(.horizontal, 8)
+
+                Button(action: {
+                    sortAction()
+                }) {
+                  
+                    Image(systemName: isAscendingOrder ? "arrow.up" : "arrow.down")
+                        .imageScale(.large)
+                   
+                }
+                .padding(.leading, 8)
             }
             .padding(.top, 8)
         }
     }
+
     var body: some View {
         TabView {
             NavigationView {
                 ScrollView {
-                    // Search Bar
-                    SearchBar(text: $searchText)
+                    // Search Bar with Sort Button
+                    SearchBar(text: $searchText, isAscendingOrder: $isAscendingOrder, sortAction: {
+                        isAscendingOrder.toggle()
 
-                    LazyVGrid(columns: columns, spacing: 16) {
-                        ForEach(filteredCards) { card in
-                            NavigationLink(destination: MTGCardView(card: card)) {
-                                CardImageView(card: card)
-                                    .frame(height: 200) // Adjust the image height as needed
+                        mtgCards.sort { (card1, card2) in
+                            if isAscendingOrder {
+                                return card1.name.lowercased() < card2.name.lowercased()
+                            } else {
+                                return card1.name.lowercased() > card2.name.lowercased()
+                            }
+                        }
+                    })
+                    
+
+                    LazyVGrid(columns: columns, spacing: 15) {
+                        ForEach(filteredCards.indices, id: \.self) { card in
+                            NavigationLink(destination: MTGCardView(card: filteredCards, currentIndex: card)) {
+                                CardImageView(card: filteredCards[card])
+                                    .frame(height: 200)
                             }
                         }
                     }
                     .padding()
-                }
-                .onAppear {
-                    // Load data from a JSON file
+                }                .onAppear {
+                    
                     if let data = loadJSON() {
                         do {
                             let decoder = JSONDecoder()
@@ -146,7 +400,7 @@ struct ContentView: View {
         }
     }
     
-    // Function to load data from a JSON file
+    
     func loadJSON() -> Data? {
         if let path = Bundle.main.path(forResource: "WOT-Scryfall", ofType: "json") {
             do {
@@ -177,13 +431,13 @@ struct CardImageView: View {
                     image
                         .resizable()
                         .aspectRatio(contentMode: .fit)
-                        .clipShape(RoundedRectangle(cornerRadius: 10)) // Adjust the corner radius as needed
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
                 case .failure:
                     Image(systemName: "exclamationmark.triangle")
                         .resizable()
                         .aspectRatio(contentMode: .fit)
                         .foregroundColor(.red)
-                        .clipShape(RoundedRectangle(cornerRadius: 10)) // Adjust the corner radius as needed
+                        .clipShape(RoundedRectangle(cornerRadius: 10)) 
                 case .empty:
                     ProgressView()
                 @unknown default:
--- W9-MobComp/MTGCard.swift
+++ W9-MobComp/MTGCard.swift
@@ -14,14 +14,14 @@ struct MTGCard: Codable, Identifiable {
     var type_line: String
     var oracle_text: String
     var image_uris: ImageURIs?
+    var legalities: Legalities?
+    var prices: PricesList?
 
-    // Define other properties as needed based on your JSON structure
 
     struct ImageURIs: Codable {
         var small: String?
         var normal: String?
         var large: String?
-        // Add other image URL properties if needed
     }
 }
 
@@ -31,3 +31,50 @@ struct MTGCardList: Codable {
     var has_more: Bool
     var data: [MTGCard]
 }
+
+struct Legalities: Codable {
+       var standard: String
+       var future: String
+       var historic: String
+       var gladiator: String
+       var pioneer: String
+       var explorer: String
+       var modern: String
+       var legacy: String
+       var pauper: String
+       var vintage: String
+       var penny: String
+       var commander: String
+       var oathbreaker: String
+       var brawl: String
+       var historicbrawl: String
+       var alchemy: String
+       var paupercommander: String
+       var duel: String
+       var oldschool: String
+       var premodern: String
+       var predh: String
+
+    
+   }
+struct PricesList: Codable {
+           var usd: String?
+           var usd_foil: String?
+           var usd_etched: String?
+           var eur: String?
+           var eur_foil: String?
+       func dictionaryRepresentation() -> [String: String] {
+                   var dictionary = [String: String]()
+
+                   if let usd = usd { dictionary["USD"] = usd }
+                   if let usd_foil = usd_foil { dictionary["USD Foil"] = usd_foil }
+                   if let usd_etched = usd_etched { dictionary["USD Etched"] = usd_etched }
+                   if let eur = eur { dictionary["EUR"] = eur }
+                   if let eur_foil = eur_foil { dictionary["EUR Foil"] = eur_foil }
+                   return dictionary
+               }
+       }
+   
+
+
+
